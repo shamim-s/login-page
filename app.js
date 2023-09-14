8a172e1ced36a10f0f4c75d44313b92cc2dfb33d
shamim-s <shamimibas@gmail.com>
2023-09-15 04:00:20 +0600

    select section added

--- app.js
+++ app.js
@@ -1,10 +1,22 @@
-// function show(value){
-//     document.querySelector('.text-value').value = value;
-//     document.querySelector(".select").classList.toggle("active");
-//     document.querySelector(".option").classList.toggle("active");
-// }
+/*Dropdown Menu*/
+$('.dropdown').click(function () {
+    $(this).attr('tabindex', 1).focus();
+    $(this).toggleClass('active');
+    $(this).find('.dropdown-menu').slideToggle(300);
+});
+$('.dropdown').focusout(function () {
+    $(this).removeClass('active');
+    $(this).find('.dropdown-menu').slideUp(300);
+});
+$('.dropdown .dropdown-menu li').click(function () {
+    $(this).parents('.dropdown').find('span').text($(this).text());
+    $(this).parents('.dropdown').find('input').attr('value', $(this).attr('id'));
+});
+/*End Dropdown Menu*/
 
-// document.querySelector(".text-value").addEventListener('click', function(){
-//     document.querySelector(".option").classList.toggle("active");
-//     document.querySelector(".select").classList.toggle("active");
-// })
+
+$('.dropdown-menu li').click(function () {
+var input = '<strong>' + $(this).parents('.dropdown').find('input').val() + '</strong>',
+  msg = '<span class="msg">Hidden input value: ';
+$('.msg').html(msg + input + '</span>');
+}); 
