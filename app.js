4ef71b6f6393cada1cd5fbb74dcce881e74fd77c
shamim-s <shamimibas@gmail.com>
2023-09-15 02:35:47 +0600

    login form icons changed

---
+++ app.js
@@ -0,0 +1,10 @@
+// function show(value){
+//     document.querySelector('.text-value').value = value;
+//     document.querySelector(".select").classList.toggle("active");
+//     document.querySelector(".option").classList.toggle("active");
+// }
+
+// document.querySelector(".text-value").addEventListener('click', function(){
+//     document.querySelector(".option").classList.toggle("active");
+//     document.querySelector(".select").classList.toggle("active");
+// })
